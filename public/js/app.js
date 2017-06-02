afa747422df8b4416177c1f6a7556c07f9d6c7ac
Kateryna Bondareva <kbondareva@gmail.com>
2017-06-02 19:20:03 -0400

    constructed incrementing counter module as well as the associated html for the user interface

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,53 @@
-console.log('yayyyy!');
+/*jshint esversion: 6*/
+const Counter = {
+  rootElement: '#app',
+  count: 0,
+  start: function(){
+    this.cacheDOM();
+    this.bindEvents();
+    this.render();
+  },
+  
+  cacheDOM: function(){
+    this.root = document.querySelector('#app');
+    this.button = document.querySelector('#increment');
+    this.output = document.querySelector('#output');
+  },
+
+  bindEvents: function(){
+    //only click events and only on the button
+    this.button.addEventListener('click', () =>{
+      console.log('Click!');
+      this.count += 1;
+      this.render();
+    });
+
+  },
+
+  render: function(){
+    this.output.textContent = this.count;
+  },
+
+};
+Counter.start();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*if (count > 0) {
+count--;
+}*/
